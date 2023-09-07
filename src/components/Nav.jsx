1d6aa4ae1f1eefd35460a47587b2b302113da842
Abhishek Ajmera <abhishekajmera17@gmail.com>
2023-09-08 00:47:14 +0530

    done

---
+++ src/components/Nav.jsx
@@ -0,0 +1,55 @@
+import { headerLogo } from '../assets/images';
+import { hamburger } from '../assets/icons';
+import { navLinks } from '../constants';
+
+const Nav = () => {
+  function openMenu() {
+    // let navlinks = document.getElementById('navLinks');
+    // navlinks.classList.remove('max-lg:hidden');
+    // navlinks.classList.add('flex-col');
+    // console.log(navlinks);
+  }
+
+  return (
+    <>
+      <header className='padding-x py-8 absolute z-10 w-full'>
+        <nav className='flex justify-between items-center max-container'>
+          <a href='/'>
+            <img
+              src={headerLogo}
+              alt='Logo'
+              width={130}
+              height={29}
+            />
+          </a>
+          <ul
+            id='navLinks'
+            className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'
+          >
+            {navLinks.map((item) => (
+              <li key={item.label}>
+                <a
+                  href={item.href}
+                  className='font-montserrat leading-normal text-lg text-slate-gray'
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className='hidden max-xl:block hover:cursor-pointer'>
+            <img
+              src={hamburger}
+              onClick={openMenu}
+              alt='Menu'
+              width={25}
+              height={25}
+            />
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Nav;
